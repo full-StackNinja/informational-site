f52b8a84cfa676a46fad98e8e7e33c8d8752fd4f
Imran Hussain <imranhussain399@gmail.com>
2024-01-01 19:13:17 +0500

    Redefine routing using express framework

--- index.js
+++ index.js
@@ -1,61 +1,31 @@
-const http = require("node:http");
-const fs = require("node:fs");
-
-const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/": {
-      handleFile("./htmlFiles/index.html", "text/html", res);
-      break;
-    }
-    case "/about": {
-      handleFile("./htmlFiles/about.html", "text/html", res);
-      break;
-    }
-    case "/contact-me": {
-      handleFile("./htmlFiles/contact-me.html", "text/html", res);
-      break;
-    }
-    case "/styles.css": {
-      handleFile("./styles.css", "text/css", res);
-      break;
-    }
-    case "/defaultStyles/meyer-reset-1st.css": {
-      handleFile("./defaultStyles/meyer-reset-1st.css", "text/css", res);
-      break;
-    }
-    case "/defaultStyles/my-css-reset-2nd.css": {
-      handleFile("./defaultStyles/my-css-reset-2nd.css", "text/css", res);
-      break;
-    }
-    case "/defaultStyles/normalize-3rd.css": {
-      handleFile("./defaultStyles/normalize-3rd.css", "text/css", res);
-      break;
-    }
-    case "/defaultStyles/typography-4th.css": {
-      handleFile("./defaultStyles/typography-4th.css", "text/css", res);
-      break;
-    }
-    default: {
-      handleFile("./htmlFiles/404.html", "text/html", res);
-    }
-  }
-});
-
-const handleFile = (path, contentType, res) => {
-  fs.readFile(path, "utf8", (err, data) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("Internal server error");
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.write(data);
-      res.end();
-    }
-  });
-};
-
+const express = require("express");
+const app = express();
+const path = require("path");
 const port = 8080;
 
-server.listen(port, () => {
-  console.log("server is listening on port: " + port);
+app.use(express.static("public/defaultStyles"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/htmlFiles", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/htmlFiles", "about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/htmlFiles", "contact-me.html"));
+});
+
+const errMessage = (req, res, next) => {
+  res.contentType("text/html");
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "public/htmlFiles", "404.html"));
+};
+
+app.use(errMessage);
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
 });
